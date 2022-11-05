da0f05aece9f08bf9b0a7edc93990a2adc2c24c6
DevAndreAkira <pianista.akira@gmail.com>
2022-11-05 10:28:57 -0300

    test: testando rota2

--- src/routes.js
+++ src/routes.js
@@ -15,20 +15,20 @@ import Contato from './Pages/Contato';
 const Router = () => {
     return (
         <BrowserRouter>
-            <Routes basename="/portfolio_devandreakira">
+            <Routes basename="/">
                 <Route path='*' element={<><div className='d-flex justify-content-center align-items-center flex-column' style={{ width: '100%', height: '100%' }}>
                     <h1 style={{ color: "#05c3c3" }}>Not Found 404</h1><img style={{ width: '250px', height: '250px' }} src={logo} alt="" /><br /><h2>Página não encontrada</h2>
                     <Link className="navbar-brand nav-link d-flex justify-content-center align-items-center"  style={{background: 'var(--secondary-color)', width: '300px'}} to="/portfolio_devandreakira">
                         <p className='py-2'>Voltar</p>
                     </Link>
                 </div></>} />
-                <Route path="/portfolio_devandreakira/" element={<Home />} />
-                <Route path="/portfolio_devandreakira/sobre" exact element={<Sobre />} />
-                <Route path="/portfolio_devandreakira/historico" exact element={<Histórico />} />
-                <Route path="/portfolio_devandreakira/habilidades" exact element={<Habilidades />} />
-                <Route path="/portfolio_devandreakira/portfolio" exact element={<Portfolio />} />
-                <Route path="/portfolio_devandreakira/hobbies" exact element={<Hobbies />} />
-                <Route path="/portfolio_devandreakira/contato" exact element={<Contato />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/sobre" exact element={<Sobre />} />
+                <Route path="/historico" exact element={<Histórico />} />
+                <Route path="/habilidades" exact element={<Habilidades />} />
+                <Route path="/portfolio" exact element={<Portfolio />} />
+                <Route path="/hobbies" exact element={<Hobbies />} />
+                <Route path="/contato" exact element={<Contato />} />
             </Routes>
         </BrowserRouter>
     );
