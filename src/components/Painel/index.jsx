5c5d8b8c753ee742f354afd62ec7043b13aadf3d
DevAndreAkira <pianista.akira@gmail.com>
2023-01-18 18:57:08 -0300

    test: ajuste header

--- src/components/Painel/index.jsx
+++ src/components/Painel/index.jsx
@@ -99,7 +99,7 @@ const Painel = () => {
                                         controls
                                     />
                                     <h2 className='painel_nome p-0 m-0 mb-1' aria-roledescription={i18n.t('dev.front')}>{i18n.t('dev.front')}</h2>
-                                    <h3 className='painel_nome p-0 m-0 mb-4'>{i18n.t('ola.ola')} André Akira</h3>
+                                    <h2 className='painel_nome p-0 m-0 mb-4'>{i18n.t('ola.ola')} André Akira</h2>
                                 </hgroup>
                                 <div className='mb-4 idiomas'>
                                     <button type="button" className="btn btn_small" onClick={handleSelectChange1} aria-label="Button" value="Inglês" title={i18n.t('btn_access.title_en')}>
